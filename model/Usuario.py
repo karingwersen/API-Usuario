40c3977e8ec91973ae8cb680f08ec32d245285d2
karingwersen <karin.ingwersen96@gmail.com>
2023-11-27 20:16:29 -0300

    Criando endpoint e implementando classe Usuario

---
+++ model/Usuario.py
@@ -0,0 +1,10 @@
+class Usuario:
+
+    def __init__(self, nome, sobrenome, e-mail, senha, data_de_nascimento, cpf, id):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.e-mail = e-mail
+        self.senha = senha
+        self.data_de_nascimento = data_de_nascimento
+        self.cpf = cpf
+        self.id = id
